cc03a623780fce1dfc29ce8894f3896ca128ca24
mhgufron <mgufron223@gmail.com>
2018-04-19 08:25:22 +0700

    add single and custom search form

--- awesome_theme/functions.php
+++ awesome_theme/functions.php
@@ -12,7 +12,7 @@ function awesome_script_enqueue() {
 	wp_enqueue_script('jquery');
 	wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.3.4', true);
 	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/awesome.js', array(), '1.0.0', true);
-	
+
 }
 
 add_action( 'wp_enqueue_scripts', 'awesome_script_enqueue');
@@ -23,12 +23,12 @@ add_action( 'wp_enqueue_scripts', 'awesome_script_enqueue');
 	==========================================
 */
 function awesome_theme_setup() {
-	
+
 	add_theme_support('menus');
-	
+
 	register_nav_menu('primary', 'Primary Header Navigation');
 	register_nav_menu('secondary', 'Footer Navigation');
-	
+
 }
 
 add_action('init', 'awesome_theme_setup');
@@ -42,6 +42,7 @@ add_theme_support('custom-background');
 add_theme_support('custom-header');
 add_theme_support('post-thumbnails');
 add_theme_support('post-formats',array('aside','image','video'));
+add_theme_support('html5',array('search-form'));
 
 /*
 	==========================================
@@ -49,9 +50,9 @@ add_theme_support('post-formats',array('aside','image','video'));
 	==========================================
 */
 function awesome_widget_setup() {
-	
+
 	register_sidebar(
-		array(	
+		array(
 			'name'	=> 'Sidebar',
 			'id'	=> 'sidebar-1',
 			'class'	=> 'custom',
@@ -62,6 +63,6 @@ function awesome_widget_setup() {
 			'after_title'   => '</h1>',
 		)
 	);
-	
+
 }
 add_action('widgets_init','awesome_widget_setup');
